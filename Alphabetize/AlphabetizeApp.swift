a410a1ca96e3945552f98200e129e37296eb0f3e
Andrew Jacob Paterson <Andrewjp97@me.com>
2024-07-30 08:23:44 -0400

    Initial Commit

---
+++ Alphabetize/AlphabetizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlphabetizeApp.swift
+//  Alphabetize
+//
+//  Created by Andrew Paterson on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct AlphabetizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
